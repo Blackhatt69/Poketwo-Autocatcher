9cc1a800f5e8face6afddb3726266dc7327f5f00
beefacke <blackhattt69@gmail.com>
2024-01-07 21:30:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+
+  
+  # Poketwo-AUTOCATCHER
+A second generation **free** and open-source Pokétwo autocatcher, created with the goal of preventing people from wasting their money on buying autocatchers online.
+
+😄😇 **Star this Repo if you find it useful**
+
+---
+
+### Youtube Tutorial
+https://www.youtube.com/watch?v=Qqog9HaNU8Y
+
+---
+
+
+### Features
+The bot has the following features:
+- ⚙️ Easy Setup (simply run a script and enter the correct information, no 'coding' needed)
+- ⬆️ Auto-levelling (Level up all your duel Pokémon to level 100 **overnight** by just selecting them!)
+- 📜 Log all the pokemons it catch in your desired channel!
+- 💲 **Completely free** and open-source (you can see the code as it is currently)
+- 💕 **Trustworthy**; this autocatcher is **completely** open-source (meaning you can see the latest code)
+- 📜 Support for all Pokémon in poketwo (GEN9 TOO!!)
+- 🏎️ Super fast; the autocatcher can even handle Incense!
+- 🔍 Pokétwo-Resistant - the autocatcher is undetectable due to it's method of catching
+- 🔊 Say command `$say <text>` to trade pokemons/manipulate bot
+- ✅ Can click any button by using `$click <messageID>`
+- 🌟 Can react to any message by using `$react <messageID>`
+
+### Requirements
+Please note that this autocatcher requires NODEJS installed on your system or in any other platform you are trying to run it onto
+
+## Invite Poke Name Bot to make the autocatcher run . [Invite Link](https://discord.com/oauth2/authorize?client_id=874910942490677270&permissions=412317379648&scope=applications.commands%20bot)
+## Alternatively You can invite Sierra too! [Invite link](https://discord.com/oauth2/authorize?client_id=696161886734909481&permissions=8&scope=bot%20applications.commands)(but i prefer only use pokename only)
+
+#### <b>Running the bot</b>
+To start up the bot for the first time, STAR THIS REPO then clone the repo by this link to your local machine or replit (wherever you wish) [click here](https://github.com/AkshatOP/Poketwo-Autocatcher.git) 
+Once you have done that hit `npm install` in console.
+
+## **Specific Channel Support**
+If you want to run the bot so that it catches only in specific channels then Head over to LINE 17 of #index.js and fill the array with channel IDs of those channel you want to catch in.
+*Leave it like [ ] if you don't want this feature* 
+
+## **Help**
+Use $help command to get info about list of commands bot have in it
+
+## **Captcha**
+If Captcha comes , The bot stops it's catching and goes to a short sleep. You can solve the captcha from any ID and then use the command `$captcha_completed` for the bot to wake up
+
+## **Config.json**
+After That enter in the following fields in **Config.json**:
+
+##### <b>TOKEN</b>:
+Paste in your discord account's user token. You can find instructions on this [here](https://www.youtube.com/watch?v=3W9tAEsK7RM)
+*CAUTION*
+If you are using repl.it and don't want your token to be leaked. Follow this [tutorial](https://www.youtube.com/watch?v=BKlv__1OoGc) to secure your token and apply that fix in line 130 of index.js. 
+
+##### <b>spamChannelID</b>:
+This will allow the bot to use your preferred channel to spam as well as catch Pokétwo spawns. Make sure you paste this carefully, as If you set it to the wrong channel it will spam and catch there.
+
+##### <b>logChannelID</b>:
+This will allow the bot to use your preferred channel to log the pokemons caught in a desired channel of yours.
+
+##### <b>errorChannelID</b>:
+This will allow the bot to use your preferred channel to log the errors of code in a desired channel of yours.
+
+##### <b>OwnerID</b>:
+This will be the user-ID of the account controlling the bot (for using "$say" and "$captcha_completed" command)  
+
+##### <b>ocrSpaceApiKey</b>:
+This is the MOST IMPORTANT part. You have to create an ocrSpace account and put it's API key. Click [here](https://ocr.space/ocrapi/freekey) to get free API-KEY
+
+After you've entered that in, the autocatcher should start successfully. (if not, check if you have entered in the right fields)
+
+> Remember to cd into your autocatcher folder as well. If you need any help with something, feel free to open a Github Issue.
+
+> ALL Done you can now start your bot by typing `node index.js` or `node .` in the console 
+
+### Auto-levelling
+To enable auto-levelling, just select the pokemon in that autocatcher ID and let it auto-level
+
+
+If you want to contribute to the community Please post the json you made after a few weeks of use of this bot here or DM me that json in Discord 🙂 
+
+
+---
+
+## **DISCLAIMER**
+
+Please note that self botting is against Discord's Terms of Service and being discovered using a self bot may result in your account being banned. To avoid this, keep knowledge of your self bot to a minimum and use a throwaway account. I am not responsible for any accounts lost due to the self bot. I also recommend checking the self bot channel's messages occasionally to see if Pokétwo has sent a captcha. **If it has, it would be a good idea to solve it.**
+
+---
---
+++ index.js
@@ -0,0 +1,307 @@
+/*
+@Developer: beefcake.officiel
+Name: Poketwo-Autocatcher
+Version: V1.3.2
+Description: bot to help users with catching pokemon
+@Supported: poketwo/pokemon
+
+*/
+const Discord = require("discord.js-selfbot-v13")
+const client = new Discord.Client({
+    checkUpdate: false
+});
+const express = require('express');
+const {
+    solveHint,
+    checkRarity
+} = require("pokehint")
+const {
+    ocrSpace
+} = require('ocr-space-api-wrapper');
+
+const config = require('./config.json')
+const json = require('./namefix.json');
+const allowedChannels = []; // Add your allowed channel IDs to this array or leave it like [] if you want to it to catch from all channels
+let isSleeping = false;
+
+
+//------------------------- KEEP-ALIVE--------------------------------//
+
+const app = express();
+if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
+app.get("/", (req, res) => {
+    res.status(200).send({
+        success: "true"
+    });
+});
+app.listen(process.env.PORT || 3000);
+
+//--------------------------------------------------------------//
+
+//-------------------------SOME EXTRA FUNCTIONS----------------------------//
+
+
+function findOutput(input) {
+    if (json.hasOwnProperty(input)) {
+        return json[input];
+    } else {
+        return input;
+    }
+}
+
+
+function checkSpawnsRemaining(string) {
+    const match = string.match(/Spawns Remaining: (\d+)/);
+    if (match) {
+        const spawnsRemaining = parseInt(match[1]);
+        console.log(spawnsRemaining)
+    }
+}
+//--------------------------------------------------------------------------//
+
+//-------------------------READY HANDLER+SPAMMER-----------------------//
+
+client.on('ready', () => {
+    console.log("Made By Beefcake.officiel")
+    console.log(`Acount: ${client.user.username} is ONLINE, `)
+    console.log("Note: When your using Incense then make sure it occurs in a separate channel where hint bots like pokename/sierra aren't enabled to send message there!")
+    console.log("Use $help to know about commands")
+
+    const channel = client.channels.cache.get(config.spamChannelID)
+
+    function getRandomInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function spam() {
+        const result = Math.random().toString(36).substring(2, 15);
+        channel.send(result + "(Made by Beefcake.officiel) ")
+        const randomInterval = getRandomInterval(1500, 5000); // Random interval for spam between 1 second and 5 seconds
+        setTimeout(spam, randomInterval);
+    }
+    spam();
+})
+
+//------------------------------------------------------------//
+
+
+//-------------------------Anti-Crash-------------------------//
+
+process.on("unhandledRejection", (reason, p) => {
+    if (reason == "Error: Unable to identify that pokemon.") {} else {
+        console.log(" [antiCrash] :: Unhandled Rejection/Catch");
+        console.log(reason, p);
+    }
+});
+process.on("uncaughtException", (err, origin) => {
+    console.log(" [antiCrash] :: Uncaught Exception/Catch");
+    console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
+    console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+    console.log(" [antiCrash] :: Multiple Resolves");
+    console.log(type, promise, reason);
+});
+
+//------------------------------------------------------------//
+
+//----------------------------AUTOCATCHER--------------------------------------//
+
+client.on('messageCreate', async message => {
+    if (message.content === "$captcha_completed" && message.author.id === config.OwnerID) {
+        isSleeping = false;
+        message.channel.send("Autocatcher Started!")
+    }
+
+    if (message.content === "$help" && message.author.id === config.OwnerID) {
+
+        await message.channel.send(
+            "``` Poketwo-Autocatcher\n Link: https://github.com/AkshatOP/Poketwo-Autocatcher\n\n $captcha_completed : Use to restart the bot once captcha is solved\n $say <content> : Make the bot say whatever you want\n $react <messageID> : React with ✅ emoji\n $click <messageID> : Clicks the button which has ✅ emoji\n $help : To show this message ```"
+        )
+    }
+
+    if (!isSleeping) {
+
+        if (message.content.includes("Please tell us") && message.author.id === "716390085896962058") {
+            isSleeping = true;
+            message.channel.send("Autocatcher Stopped , Captcha Detected! Use `$captcha_completed` once the captcha is solved ");
+            setTimeout(async function() {
+                isSleeping = false
+            }, 18000000) //5 hours
+
+
+
+        } else if (message.content.startsWith("$say") && message.author.id == config.OwnerID) {
+            let say = message.content.split(" ").slice(1).join(" ")
+            message.channel.send(say)
+
+        } else if (message.content.startsWith("$react") && message.author.id == config.OwnerID) {
+            let msg
+            try {
+                const args = message.content.slice(1).trim().split(/ +/g)
+                msg = await message.channel.messages.fetch(args[1])
+            } catch (err) {
+                message.reply(`Please Specify the message ID as an arguement like "$react <messageID>"`)
+            }
+            if (msg) {
+                try {
+                    msg.react("✅")
+                    message.react("✅")
+                } catch (err) {
+                    message.react("❌")
+                    console.log(err)
+                }
+            }
+
+
+        } else if (message.content.startsWith("$click") && message.author.id == config.OwnerID) {
+
+            let msg
+            try {
+                var args = message.content.slice(1).trim().split(/ +/g)
+                msg = await message.channel.messages.fetch(args[1])
+            } catch (err) {
+                message.reply(`Please Specify the message ID as an arguement like "$click <messageID>".`)
+            }
+
+            if (msg) {
+                try {
+                    await msg.clickButton();
+                    message.react("✅")
+                } catch (err) {
+                    message.react("❌")
+                    console.log(err)
+                }
+            }
+        } else if (message.content == "That is the wrong pokémon!" && message.author.id == "716390085896962058") {
+            message.channel.send(`<@716390085896962058> h`)
+        } else if (message.author.id == "716390085896962058") {
+            if (message?.embeds[0]?.footer?.text.includes("Spawns Remaining")) {
+                await message.channel.send(`<@716390085896962058> h`)
+                if ((message.embeds[0]?.footer?.text == "Incense: Active.\nSpawns Remaining: 0.")) {
+                    message.channel.send(`<@716390085896962058> buy incense`)
+                }
+
+            } else if (message.content.includes("The pokémon is")) {
+                let rarity;
+                const pokemon = await solveHint(message)
+                console.log(`Catching ${pokemon}`)
+                await message.channel.send(`<@716390085896962058> c ${pokemon}`)
+
+                console.log("[" + message.guild.name + "/#" + message.channel.name + "] " + pokemon)
+                try{
+                 rarity = await checkRarity(`${pokemon}`)
+                } catch {
+                    rarity = "Not Found in Database";
+                }
+
+                const channel6 = client.channels.cache.get(config.logChannelID)
+                channel6.send("[" + message.guild.name + "/#" + message.channel.name + "] " + "**__" + pokemon + "__** " + "Rarity " + rarity + " made by 🔥⃤•AK_ØPᵈᵉᵛ✓#6326")
+
+
+            }
+
+        } else {
+
+            const Pokebots = ["696161886734909481", "874910942490677270"]; //sierra ,pokename
+            if (allowedChannels.length > 0 && !allowedChannels.includes(message.channel.id)) {
+                return;
+            }
+            if (Pokebots.includes(message.author.id)) {
+                let preferredURL = null;
+                message.embeds.forEach((e) => {
+                    if (e.image) {
+                        const imageURL = e.image.url;
+                        if (imageURL.includes("prediction.png")) {
+                            preferredURL = imageURL;
+                        } else if (imageURL.includes("embed.png") && !preferredURL) {
+                            preferredURL = imageURL;
+                        }
+                    }
+                });
+
+                if (preferredURL) {
+                    let url = preferredURL;
+
+
+                    async function main() {
+                        try {
+                            const res1 = await ocrSpace(url, {
+                                apiKey: `${config.ocrSpaceApiKey}`
+                            });
+                            const name1 = res1.ParsedResults[0].ParsedText.split('\r')[0];
+                            const name5 = name1.replace(/Q/g, 'R');
+                            const name = findOutput(name5);
+
+                            const delay = Math.floor(Math.random() * 6 + 5) * 1000; //interval from 5-10seconds
+                            console.log("A Pokemon Spawned, Catching in " + (delay / 1000) + "seconds")
+                            setTimeout(async () => {
+                                message.channel.send(`<@716390085896962058> c ${name}`)
+                                    .then(a => {}).catch(error => {
+                                        console.error(error);
+                                        const channel = client.channels.cache.get(config.errorChannelID)
+                                        channel.send(error)
+                                    })
+
+
+                                const filter = (msg) => msg.author.id === "716390085896962058";
+                                const collector = new Discord.MessageCollector(message.channel, filter, {
+                                    max: 1,
+                                    time: 13000
+                                }); // Collect only one message in 10 seconds
+
+                                collector.on('collect', async (collected) => {
+
+
+                                    if (collected.content.includes("Congratulations")) {
+
+                                        function capitalizeFirstLetter(str) {
+                                            return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+                                        }
+
+                                        let rareity;
+                                        const name2 = capitalizeFirstLetter(name)
+                                        try{
+                                        rareity = await checkRarity(`${name2}`)
+                                        } catch {
+                                            rareity = "Not Found in Database"
+                                        }
+                                        const logchannel = client.channels.cache.get(config.logChannelID)
+                                        logchannel.send("[" + collected.guild.name + "/#" + collected.channel.name + "] " + "**__" + name2 + "__** " + "Rarity " + rareity + " made by 🔥⃤•AK_ØPᵈᵉᵛ✓#6326").then(b => {}).catch(error => {
+
+                                            console.error(error);
+                                            const channel = client.channels.cache.get(config.errorChannelID)
+                                            channel.send(error)
+                                        })
+
+                                        collector.stop();
+
+
+                                    }
+
+
+
+                                });
+
+
+                            }, delay);
+
+
+                        } catch (error) {
+                            console.error(error);
+                            const channel = client.channels.cache.get(config.errorChannelID)
+                            channel.send(error)
+                        }
+                    }
+                    main()
+                }
+
+            }
+
+        }
+    }
+})
+client.login(config.TOKEN) //use process.env.TOKEN if you are using it in repl.it
